bc2662267e5e9cee937a8ede8d0b411daad6765c
Akshay Katrodiya <akshaykatrodiya23@gmail.com>
2018-05-14 00:40:36 -0400

    home component for better UI

---
+++ src/app/models/home.ts
@@ -0,0 +1,28 @@
+export class Home{
+    allExercises: string[] = [
+        "Barbell Press",
+        "DB Press & DB Fly",
+        "Barbell Shrugs",
+        "High Angle Cable Curl",
+        "Bench Dips",
+        "Close Grip Machine Rows",
+        "Hyper Back Extension",
+        "DB Side Raise",
+        "Laying Rear Delt Fly",
+        "Wide Grip Barbell Pull Down",
+        "Heck Squat",
+        "Leg Presses",
+        "Barbell Step-ups",
+        "Walking Lunges with Squat",
+        "Cardio",
+        "Stationary Bike",
+        "Crunches on Lat Machine",
+        "Side to Side Twist",
+        "Incline Crunches with Twist",
+        "Flutter Kicks",
+        "Rope Lat Pull Down",
+        "Biceps 21 & Triceps 21",
+        "Russian Twist",
+        "Incline Cable Fly"
+    ];
+}
